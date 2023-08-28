5a102d562435f422ad8bfd5972c1febe9e7196e1
nverma283 <143256029+nverma283@users.noreply.github.com>
2023-08-28 23:14:29 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <nverma283>
    # Input:
    # p, principal amount
    # t, time period in years
